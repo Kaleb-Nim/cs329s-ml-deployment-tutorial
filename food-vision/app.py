6ecfa3d99c1aefc815dfc10ff87feccc1bec0037
yuwotmate <kaleb.nim@gmail.com>
2022-08-22 14:19:57 +0800

    update

--- food-vision/app.py
+++ food-vision/app.py
@@ -5,17 +5,62 @@ import requests
 import SessionState
 import streamlit as st
 import tensorflow as tf
-from utils import load_and_prep_image, classes_and_models, update_logger, predict_json
+from utils import load_and_prep_image, classes_and_models, update_logger, predict_json,canvas_result_processing,decode_batch_predictions
+from streamlit_drawable_canvas import st_canvas
+from PIL import Image
+import pandas as pd
+import time
+import base64
+from pathlib import Path
+from io import BytesIO
+import uuid
 
 # Setup environment credentials (you'll need to change these)
 # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "daniels-dl-playground-4edbcb2e6e37.json" # change for your GCP key
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "food-vision-v2-6e0120b30d3e.json"
-PROJECT = "food-vision-v2" # change for your GCP project
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "handwritten-recognition-356808-29840b51851f.json"
+
+PROJECT = "handwritten-recognition-356808" # change for your GCP project
 REGION = "us-central1" # change for your GCP region (where your model is hosted)
 
 ### Streamlit code (works as a straigtht-forward script) ###
-st.title("Welcome to Food Vision 🍔📸")
-st.header("Identify what's in your food photos!")
+st.title("Welcome to Handwritten Recognition 🍔📸")
+st.header("Finally, teacher's can't say they cant read my handwriting -Kaleb")
+
+drawing_mode = st.sidebar.selectbox(
+    "Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
+)
+if drawing_mode == 'point':
+    point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
+stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+stroke_color = st.sidebar.color_picker("Stroke color hex: ")
+realtime_update = st.sidebar.checkbox("Update in realtime", True)
+
+
+# Create a canvas component
+canvas_result = st_canvas(
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    stroke_width=stroke_width,
+    stroke_color=stroke_color,
+
+    update_streamlit=realtime_update,
+    height=150,
+    
+    drawing_mode=drawing_mode,
+    point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
+    key="png_export",
+)
+
+import cv2 
+import numpy as np
+
+image_width = 128
+image_height = 32
+img_size=(image_width, image_height)
+
+# some datapreprocessing
+if canvas_result.json_data is not None:
+    image_tensor_processes = canvas_result_processing(canvas_result)
+    st.write(image_tensor_processes)
 
 @st.cache # cache the function so predictions aren't always redone (Streamlit refreshes every click)
 def make_prediction(image, model, class_names):
@@ -28,40 +73,50 @@ def make_prediction(image, model, class_names):
      pred_class (prediction class from class_names)
      pred_conf (model confidence)
     """
-    image = load_and_prep_image(image)
+    # image = load_and_prep_image(image)
     # Turn tensors into int16 (saves a lot of space, ML Engine has a limit of 1.5MB per request)
-    image = tf.cast(tf.expand_dims(image, axis=0), tf.int16)
+    # image = tf.cast(tf.expand_dims(image, axis=0), tf.int16)
+    # image = tf.cast(tf.expand_dims(image, axis=0), tf.int16)
     # image = tf.expand_dims(image, axis=0)
     preds = predict_json(project=PROJECT,
                          region=REGION,
                          model=model,
                          instances=image)
-    pred_class = class_names[tf.argmax(preds[0])]
-    pred_conf = tf.reduce_max(preds[0])
-    return image, pred_class, pred_conf
+    print(f'This is preds: {preds}')
+    # pred_class = class_names[tf.argmax(preds[0])]
+    # pred_conf = tf.reduce_max(preds[0])
+    return image, preds
 
-# Pick the model version
-choose_model = st.sidebar.selectbox(
-    "Pick model you'd like to use",
-    ("Model 1 (10 food classes)", # original 10 classes
-     "Model 2 (11 food classes)", # original 10 classes + donuts
-     "Model 3 (11 food classes + non-food class)") # 11 classes (same as above) + not_food class
-)
+# # Pick the model version
+# choose_model = st.sidebar.selectbox(
+#     "Pick model you'd like to use",
+#     ("Model 1 (10 food classes)", # original 10 classes
+#      "Model 2 (11 food classes)", # original 10 classes + donuts
+#      "Model 3 (11 food classes + non-food class)"), # 11 classes (same as above) + not_food class
+#      "Handwriting model"
+# )
 
-# Model choice logic
-if choose_model == "Model 1 (10 food classes)":
-    CLASSES = classes_and_models["model_1"]["classes"]
-    MODEL = classes_and_models["model_1"]["model_name"]
-elif choose_model == "Model 2 (11 food classes)":
-    CLASSES = classes_and_models["model_2"]["classes"]
-    MODEL = classes_and_models["model_2"]["model_name"]
-else:
-    CLASSES = classes_and_models["model_3"]["classes"]
-    MODEL = classes_and_models["model_3"]["model_name"]
+CLASSES = classes_and_models["Handwriting_model"]["classes"]
+MODEL = classes_and_models["Handwriting_model"]["model_name"] 
+
+# # Model choice logic
+# if choose_model == "Model 1 (10 food classes)":
+#     CLASSES = classes_and_models["model_1"]["classes"]
+#     MODEL = classes_and_models["model_1"]["model_name"]
+# elif choose_model == "Model 2 (11 food classes)":
+#     CLASSES = classes_and_models["model_2"]["classes"]
+#     MODEL = classes_and_models["model_2"]["model_name"]
+# elif choose_model == "Handwriting model":
+#     CLASSES = classes_and_models["Handwriting_model"]["classes"]
+#     MODEL = classes_and_models["Handwriting_model"]["model_name"]    
+# else:
+#     CLASSES = classes_and_models["model_3"]["classes"]
+#     MODEL = classes_and_models["model_3"]["model_name"]
 
 # Display info about model and classes
 if st.checkbox("Show classes"):
     st.write(f"You chose {MODEL}, these are the classes of food it can identify:\n", CLASSES)
+st.write(f"You chose {MODEL}, these are the classes of food it can identify:\n", CLASSES)
 
 # File uploader allows user to add their own image
 uploaded_file = st.file_uploader(label="Upload an image of food",
@@ -71,24 +126,24 @@ uploaded_file = st.file_uploader(label="Upload an image of food",
 # See: https://discuss.streamlit.io/t/the-button-inside-a-button-seems-to-reset-the-whole-app-why/1051/11 
 session_state = SessionState.get(pred_button=False)
 
-# Create logic for app flow
-if not uploaded_file:
-    st.warning("Please upload an image.")
-    st.stop()
-else:
-    session_state.uploaded_image = uploaded_file.read()
-    st.image(session_state.uploaded_image, use_column_width=True)
-    pred_button = st.button("Predict")
+# # Create logic for app flow
+# if not uploaded_file:
+#     st.warning("Please upload an image.")
+#     st.stop()
+# else:
+#     session_state.uploaded_image = uploaded_file.read()
+#     st.image(session_state.uploaded_image, use_column_width=True)
+#     pred_button = st.button("Predict")
 
-# Did the user press the predict button?
+pred_button = st.button("Predict")
+# # Did the user press the predict button?
 if pred_button:
     session_state.pred_button = True 
 
 # And if they did...
 if session_state.pred_button:
-    session_state.image, session_state.pred_class, session_state.pred_conf = make_prediction(session_state.uploaded_image, model=MODEL, class_names=CLASSES)
-    st.write(f"Prediction: {session_state.pred_class}, \
-               Confidence: {session_state.pred_conf:.3f}")
+    session_state.image, session_state.pred_value= make_prediction(image_tensor_processes, model=MODEL, class_names=CLASSES)
+    st.write(f"Prediction: {session_state.pred_value}")
 
     # Create feedback mechanism (building a data flywheel)
     session_state.feedback = st.selectbox(
--- food-vision/utils.py
+++ food-vision/utils.py
@@ -1,7 +1,29 @@
 # Utils for preprocessing data etc 
+import base64
+import json
+import os
+import re
+from io import BytesIO
+from pathlib import Path
+
 import tensorflow as tf
 import googleapiclient.discovery
 from google.api_core.client_options import ClientOptions
+
+import streamlit as st
+from PIL import Image
+from streamlit_drawable_canvas import st_canvas
+from svgpathtools import parse_path
+
+from tensorflow.keras.layers.experimental.preprocessing import StringLookup
+# import tensorflow_io as tfio
+from tensorflow import keras
+import cv2 
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import os
 
 base_classes = ['chicken_curry',
  'chicken_wings',
@@ -19,6 +41,10 @@ classes_and_models = {
         "classes": base_classes,
         "model_name": "food_vision_model_1_10_class_v2" # change to be your model name
     },
+    "Handwriting_model":{
+        'classes': "Nil",
+        "model_name": "handwitten_word_recognition" # change to be your model name
+    },
     "model_2": {
         "classes": sorted(base_classes + ["donut"]),
         "model_name": "efficientnet_model_2_11_classes"
@@ -28,8 +54,11 @@ classes_and_models = {
         "model_name": "efficientnet_model_3_12_classes"
     }
 }
+image_width = 128
+image_height = 32
 
 def predict_json(project, region, model, instances, version=None):
+    
     """Send json data to a deployed model for prediction.
 
     Args:
@@ -44,7 +73,7 @@ def predict_json(project, region, model, instances, version=None):
         Mapping[str: any]: dictionary of prediction results defined by the 
             model.
     """
-    # Create the ML Engine service object
+    # Create the ML Engine service object 
     prefix = "{}-ml".format(region) if region else "ml"
     api_endpoint = "https://{}.googleapis.com".format(prefix)
     client_options = ClientOptions(api_endpoint=api_endpoint)
@@ -54,13 +83,17 @@ def predict_json(project, region, model, instances, version=None):
     if version is not None:
         model_path += "/versions/{}".format(version)
 
+
     # Create ML engine resource endpoint and input data
     ml_resource = googleapiclient.discovery.build(
         "ml", "v1", cache_discovery=False, client_options=client_options).projects()
     instances_list = instances.numpy().tolist() # turn input into list (ML Engine wants JSON)
     
+
     input_data_json = {"signature_name": "serving_default",
-                       "instances": instances_list} 
+                       "instances": instances_list}
+
+    
 
     request = ml_resource.predict(name=model_path, body=input_data_json)
     response = request.execute()
@@ -106,3 +139,163 @@ def update_logger(image, model_used, pred_class, pred_conf, correct=False, user_
         "user_label": user_label
     }   
     return logger
+
+
+
+#  Steamlit 
+AUTOTUNE = tf.data.AUTOTUNE
+characters = ['!',
+'"',
+ '#',
+ '&',
+ "'",
+ '(',
+ ')',
+ '*',
+ '+',
+ ',',
+ '-',
+ '.',
+ '/',
+ '0',
+ '1',
+ '2',
+ '3',
+ '4',
+ '5',
+ '6',
+ '7',
+ '8',
+ '9',
+ ':',
+ ';',
+ '?',
+ 'A',
+ 'B',
+ 'C',
+ 'D',
+ 'E',
+ 'F',
+ 'G',
+ 'H',
+ 'I',
+ 'J',
+ 'K',
+ 'L',
+ 'M',
+ 'N',
+ 'O',
+ 'P',
+ 'Q',
+ 'R',
+ 'S',
+ 'T',
+ 'U',
+ 'V',
+ 'W',
+ 'X',
+ 'Y',
+ 'Z',
+ 'a',
+ 'b',
+ 'c',
+ 'd',
+ 'e',
+ 'f',
+ 'g',
+ 'h',
+ 'i',
+ 'j',
+ 'k',
+ 'l',
+ 'm',
+ 'n',
+ 'o',
+ 'p',
+ 'q',
+ 'r',
+ 's',
+ 't',
+ 'u',
+ 'v',
+ 'w',
+ 'x',
+ 'y',
+ 'z']
+# Mapping characters to integers.
+char_to_num = StringLookup(vocabulary=list(characters), mask_token=None)
+
+# Mapping integers back to original characters.
+num_to_char = StringLookup(
+    vocabulary=char_to_num.get_vocabulary(), mask_token=None, invert=True
+)
+
+max_len = 21
+def decode_batch_predictions(pred):
+    input_len = np.ones(pred.shape[0]) * pred.shape[1]
+    # Use greedy search. For complex tasks, you can use beam search.
+    results = keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0][
+        :, :max_len
+    ]
+    # Iterate over the results and get back the text.
+    output_text = []
+    for res in results:
+        res = tf.gather(res, tf.where(tf.math.not_equal(res, -1)))
+        res = tf.strings.reduce_join(num_to_char(res)).numpy().decode("utf-8")
+        output_text.append(res)
+    return output_text
+
+
+def distortion_free_resize(image, img_size):
+    w, h = img_size
+    image = tf.image.resize(image, size=(h, w), preserve_aspect_ratio=True)
+
+    # Check tha amount of padding needed to be done.
+    pad_height = h - tf.shape(image)[0]
+    pad_width = w - tf.shape(image)[1]
+
+    # Only necessary if you want to do same amount of padding on both sides.
+    if pad_height % 2 != 0:
+        height = pad_height // 2
+        pad_height_top = height + 1
+        pad_height_bottom = height
+    else:
+        pad_height_top = pad_height_bottom = pad_height // 2
+
+    if pad_width % 2 != 0:
+        width = pad_width // 2
+        pad_width_left = width + 1
+        pad_width_right = width
+    else:
+        pad_width_left = pad_width_right = pad_width // 2
+
+    image = tf.pad(
+        image,
+        paddings=[
+            [pad_height_top, pad_height_bottom],
+            [pad_width_left, pad_width_right],
+            [0, 0],
+        ],
+    )
+
+    image = tf.transpose(image, perm=[1, 0, 2])
+    image = tf.image.flip_left_right(image)
+    return image
+
+image_width = 128
+image_height = 32
+
+def canvas_result_processing(canvas_result,img_size=(image_width, image_height)):
+    img = canvas_result.image_data.astype(np.uint8)
+    rgba = tf.unstack(img, axis=-1)
+    r, g, b, a = rgba[0], rgba[1], rgba[2], rgba[3]
+    img_rgb = tf.stack([r, g, b], axis=-1)
+
+    img_grayscaled = tf.image.rgb_to_grayscale(img_rgb, name=None)
+    image = distortion_free_resize(img_grayscaled, img_size)
+    image = tf.cast(image, tf.float32) / 255.0
+
+    output_image = tf.expand_dims(image, axis=0) # expand image dimensions (224, 224, 3) -> (1, 224, 224, 3) 
+    # display output_image
+    # print(output_image.shape)
+    return output_image
